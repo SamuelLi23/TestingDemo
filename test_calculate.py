f7f84b91fba289d6ae2d77b5074ea9d140ae3f24
Samuel Li <SamuelLi@Samuels-MacBook-Pro-2.local>
2019-09-18 12:18:53 -0400

    My testing files are ready to go

---
+++ test_calculate.py
@@ -0,0 +1,21 @@
+import pytest
+
+@pytest.mark.parametrize("a, b, expected", [
+(2, 3, 5),
+(3, 7, 10),
+(-2, 5, 3),
+(2, 10, 12),
+(0.1, 0.2, 0.3),
+])
+
+def test_add(a, b, expected):
+    from calculate import add
+    result = add(a, b)
+    assert result == pytest.approx(expected)
+
+
+
+def test_sub(a, b, expected):
+    from calculate import sub
+    result = sub(a, b)
+    assert result == pytest.approx(expected)
